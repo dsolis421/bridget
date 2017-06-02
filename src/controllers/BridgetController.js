163d412d92687befb3a60d9ed38b700d8059ff27
dsolis421 <snapshot8d@gmail.com>
2017-06-01 21:12:33 -0500

    v1 initial draft of monthly budget

--- src/controllers/BridgetController.js
+++ src/controllers/BridgetController.js
@@ -1,5 +1,5 @@
 app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $rootScope) {
-  $scope.greeting = "Hi, I\'m Bridget and all I do is budget!";
+  $scope.greeting = "Hi, I\'m Bridget and all I can help answer some of your most important financial questions.";
   $scope.face = 'img/bettie_normal.png';
   $scope.appchoices = {
     type: 'select',
@@ -39,7 +39,14 @@ app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $r
     thankyou: "Thank you!",
     yourewelcome: "You're very welcome",
     imsorry: "I'm sorry",
-    mortgage_response: "I can estimate a monthly mortgage payment if you can provide the loan amount, the interest rate, and the loan term (in years)"
+    mortgage_response: "I can estimate a monthly mortgage payment if you can provide the loan amount,\
+     the interest rate, and the loan term (in years)",
+    budget_response: "I can help you determine the health of your monthly budget.\
+      Enter an amount for how much income you receive per month. You can enter multiple items for income, if needed.\
+      Be sure to click Add to Income for each entry to add them to the budget calculation.\
+      Do the same for each monthly debt payment. This could be rent payments, personal loans, or car payments.\
+      Again, be sure to click Add to Debt after each entry.  You can add as many entries for income and debt as needed.\
+      Once you have all your entries for income and debt, click Calculate Budget to receive an analysis of your monthly budget."
   };
   $scope.updateBridget = function(look, response) {
     $scope.face = $scope.looks[look];
@@ -48,7 +55,7 @@ app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $r
   $scope.showBudget = function() {
     $rootScope.budget = true;
     $rootScope.mortgage = false;
-    $scope.updateBridget('normal_smile','helpful_affirm');
+    $scope.updateBridget('thinking_right','budget_response');
   };
   $scope.showMortgage = function() {
     $rootScope.budget = false;
--- src/controllers/BudgetController.js
+++ src/controllers/BudgetController.js
@@ -1,25 +1,43 @@
 app.controller('BudgetController',['$scope','$rootScope', function($scope, $rootScope) {
   $scope.title = 'Monthly Budget';
-  $scope.incomedesc = '';
-  $scope.incomeamount;
-  $scope.debtdesc = '';
-  $scope.debtamount;
+  $scope.budgetdesc = '';
+  $scope.budgetamount;
   $scope.income = [];
   $scope.debt = [];
   $scope.ratio;
+  $scope.disposable;
+  $scope.analysis_response = '';
   $scope.addToIncome = function() {
-    var incomeentry = {};
-    incomeentry.description = $scope.incomedesc;
-    incomeentry.amount = $scope.incomeamount;
+    var incomeentry = { id: Math.floor(Math.random() * 99999),
+      description: $scope.budgetdesc,
+      amount: $scope.budgetamount };
     $scope.income.push(incomeentry);
-    $scope.incomedesc = '';
-    $scope.incomeamount = null;
+    $scope.budgetdesc = '';
+    $scope.budgetamount = null;
   };
   $scope.addToDebt = function() {
-    var debtentry = { description: $scope.debtdesc, amount: $scope.debtamount };
+    var debtentry = { id: Math.floor(Math.random() * 99999),
+      description: $scope.budgetdesc,
+      amount: $scope.budgetamount };
     $scope.debt.push(debtentry);
-    $scope.debtdesc = '';
-    $scope.debtamount = null;
+    $scope.budgetdesc = '';
+    $scope.budgetamount = null;
+  };
+  $scope.removeIncome = function (index) {
+    console.log('remove fired', $scope.income[index].id);
+    var newincome = $scope.income.filter(function(x) {
+      console.log(x.id);
+      return x.id !== $scope.income[index].id;
+    });
+    $scope.income = newincome;
+  };
+  $scope.removeDebt = function (index) {
+    console.log('remove fired', $scope.debt[index].id);
+    var newdebt = $scope.debt.filter(function(x) {
+      console.log(x.id);
+      return x.id !== $scope.debt[index].id;
+    });
+    $scope.debt = newdebt;
   };
   $scope.sumEntries = function(arr) {
     var sum = 0;
@@ -33,5 +51,35 @@ app.controller('BudgetController',['$scope','$rootScope', function($scope, $root
   $scope.calcDebtToIncome = function() {
     $scope.ratio = (($scope.sumEntries($scope.debt) / $scope.sumEntries($scope.income)) * 100).toFixed(2);
     console.log($scope.ratio);
+  };
+  $scope.calcDispIncome = function() {
+    $scope.disposable = ($scope.sumEntries($scope.income) - $scope.sumEntries($scope.debt)).toFixed(2);
+    console.log($scope.disposable);
+  };
+  $scope.analyzeAndRepond = function () {
+    if ($scope.ratio <= 30) {
+      $scope.analysis_response = 'Your debt to income ratio looks really good.\
+        Which means your income far surpasses the amount of monthly debt payments you are required to make.\
+        You are in a good position to make a major purchase or take on more debt.'
+    } else if ($scope.ratio > 30 && $scope.ratio <= 45) { $scope.analysis_response =
+      'Your debt to income ratio is right about where it should be.\
+       The amount of income you receive is sufficent to cover your monthly debt payments, while providing extra disposable\
+       income for other purchases.  More importantly, it leaves room for emergency expenses like doctor visits or car repairs.'
+    } else if ($scope.ratio > 45 && $scope.ratio <= 70) { $scope.analysis_response =
+      'Your debt to income ratio could use some improvement.  While there\'s no immediate need for concern, you will have a hard time\
+       affording a major purchase or qualifying for a new loan.  Ideally, your debt to income ratio should be closer to 43%.  It would be\
+       wise to consider paying down some of your debt. Consider also that a little more disposable income will help with\
+       unexpected expenses like car repairs or doctor visits.'
+    }  else { $scope.analysis_response =
+      'Your debt to income ratio needs some work.  Your income may not be enough to cover your monthly debt payments, which\
+       could could strain your monthly budget. But don\'t stress, you can look for opportunities to refinance your debt in a way\
+       that lowers your monthly payments.  You could also consider eliminating monthly expenses that are not as critical.\
+       Your goal should be to reduce your DTI as much as possible.  You can do it!'
+    };
+  };
+  $scope.handleBudget = function() {
+    $scope.calcDebtToIncome();
+    $scope.calcDispIncome();
+    $scope.analyzeAndRepond();
   };
 }]);
