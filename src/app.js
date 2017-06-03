b0c2bc19c92434e7cfa92ad873aba5a4525210ff
dsolis421 <snapshot8d@gmail.com>
2017-06-03 17:59:21 -0500

    v1 added about

--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ var app = angular.module('bridget-app',[]);
 app.run(function ($rootScope) {
   $rootScope.budget = false;
   $rootScope.mortgage = false;
+  $rootScope.about = false;
 });
--- src/controllers/BridgetController.js
+++ src/controllers/BridgetController.js
@@ -1,5 +1,5 @@
 app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $rootScope) {
-  $scope.greeting = "Hi, I\'m Bridget and all I can help answer some of your most important financial questions.";
+  $scope.greeting = "Hi, I\'m Bridget and I can help answer some of your most important financial questions.";
   $scope.face = 'img/bettie_normal.png';
   $scope.appchoices = {
     type: 'select',
@@ -25,20 +25,14 @@ app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $r
     helpful_affirm: "Sure, I can do that!",
     helpful_negative: "Sorry, I wish I could.",
     sarcastic_affirm: "Naturally, I live for this.",
-    sarcastic_negative: "Let's not and say that we did.",
+    sarcastic_negative: "You could always get a pencil and do this yourself. Oh wait...",
     angry_affirm: "Hell yes!",
     angry_negative: "Hell no!",
-    learn_income: "Let\'s determine your monthly income.",
-    have_job: "Do you have a job?",
-    how_often: "How often do you get paid?",
-    other_income: "Do you have any other income that you want to include, like child support or alimony?",
     learn_debt: "Now, let's determine your monthly bill payments. This is any monthly payment you make for outstandng debt, such as car payments, rent/mortgage, or credit cards.",
-    great_ratio: "Your income compared to your debt is excellent. You can afford to take on more debt, if you wish.",
-    good_ratio: "Your income compared to your debt is right where it should be. I would not recommend taking on more debt at this time.",
-    bad_ratio: "Your debt is high compared to your income. I would strongly recommend you pay down some of your debt.",
     thankyou: "Thank you!",
-    yourewelcome: "You're very welcome",
+    yourewelcome: "You're very welcome.",
     imsorry: "I'm sorry",
+    sure: "The numbers dont\'t lie.",
     mortgage_response: "I can estimate a monthly mortgage payment if you can provide the loan amount,\
      the interest rate, and the loan term (in years)",
     budget_response: "I can help you determine the health of your monthly budget.\
@@ -46,7 +40,8 @@ app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $r
       Be sure to click Add to Income for each entry to add them to the budget calculation.\
       Do the same for each monthly debt payment. This could be rent payments, personal loans, or car payments.\
       Again, be sure to click Add to Debt after each entry.  You can add as many entries for income and debt as needed.\
-      Once you have all your entries for income and debt, click Calculate Budget to receive an analysis of your monthly budget."
+      Once you have all your entries for income and debt, click Calculate Budget to receive an analysis of your monthly budget.",
+    about: "I'm shy..."
   };
   $scope.updateBridget = function(look, response) {
     $scope.face = $scope.looks[look];
@@ -55,12 +50,20 @@ app.controller('BridgetController', ['$scope', '$rootScope', function($scope, $r
   $scope.showBudget = function() {
     $rootScope.budget = true;
     $rootScope.mortgage = false;
+    $rootScope.about = false;
     $scope.updateBridget('thinking_right','budget_response');
   };
   $scope.showMortgage = function() {
     $rootScope.budget = false;
     $rootScope.mortgage = true;
+    $rootScope.about = false;
     $scope.updateBridget('normal_smile','mortgage_response');
+  };
+  $scope.showAbout = function () {
+    $rootScope.budget = false;
+    $rootScope.mortgage = false;
+    $rootScope.about = true;
+    $scope.updateBridget('talk_right','about');
   };
   $scope.changeBridget = function() {
     if($scope.appchoices.value === 'Estimate Mortgage Payment') {
